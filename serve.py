f0d4e1e778c455bc1aad468f4013090e7b643b8e
Martin Pärtel <martin.partel@gmail.com>
2022-09-18 09:58:32 +0300

    serve.py: worked around Keras multithreading issues by loading and running the model in a single worker thread.

--- serve.py
+++ serve.py
@@ -1,5 +1,6 @@
-import os
+from queue import Queue, SimpleQueue
 import sys
+from threading import Thread
 import unicodedata
 
 from flask import Flask, request
@@ -23,17 +24,64 @@ def tag():
     data = request.get_json() if request.method == 'POST' else request.values
     text = data['text']
     tokenized = data.get('tokenized') in ('1', 'True', 'true')
-    return app.tagger.tag(text, tokenized)
+    return app.tagger_thread.tag(text, tokenized)
 
+
+# We're having trouble using the model from multiple threads:
+#   https://github.com/keras-team/keras/issues/11290
+#   https://github.com/keras-team/keras/issues/5223
+# Probably upgrading to TensorFlow 2 or something would fix it,
+# but I don't know all the trouble that would entail, so we use a worker thread.
+class TaggerThread(Thread):
+    def __init__(self, model_dir):
+        super().__init__(daemon=True)
+        self.model_dir = model_dir
+        self.queue = Queue(maxsize=2)
+
+    def run(self):
+        tagger = Tagger.load(self.model_dir)
+        while True:
+            command = self.queue.get()
+            try:
+                if command['type'] == 'ping':
+                    command['callback']('pong')
+                if command['type'] == 'tag':
+                    text = command['text']
+                    tokenized = command['tokenized']
+                    result = tagger.tag(text, tokenized)
+                    command['callback'](result)
+            except BaseException as err:
+                command['error_callback'](err)
+            finally:
+                self.queue.task_done()
+
+    def ping(self):
+        return self._send_command('ping', {})
+
+    def tag(self, text, tokenized):
+        return self._send_command('tag', {'text': text, 'tokenized': tokenized})
+
+    def _send_command(self, type, args):
+        response_queue = SimpleQueue()
+        command = args.copy()
+        command['type'] = type
+        command['callback'] = lambda x: response_queue.put(['ok', x])
+        command['error_callback'] = lambda e: response_queue.put(['err', e])
+        self.queue.put(command)
+        resp = response_queue.get()
+        if resp[0] == 'ok':
+            return resp[1]
+        else:
+            raise resp[1]
 
 class Tagger(object):
-    def __init__(self, model, tokenizer, labels, config):
+    def __init__(self, session, graph, model, tokenizer, labels, config):
+        self.session = session
+        self.graph = graph
         self.model = model
         self.tokenizer = tokenizer
         self.labels = labels
         self.config = config
-        self.session = None
-        self.graph = None
 
     def tag(self, text, tokenized=False):
         max_seq_len = self.config['max_seq_length']
@@ -45,19 +93,16 @@ class Tagger(object):
         dummy = ['O'] * len(words)
         data = process_sentences([words], [dummy], self.tokenizer, max_seq_len)
         x = encode(data.combined_tokens, self.tokenizer, max_seq_len)
-        if self.session is None or self.graph is None:
-            probs = self.model.predict(x, batch_size=8)    # assume singlethreaded
-        else:
-            with self.session.as_default():
-                with self.graph.as_default():
-                    probs = self.model.predict(x, batch_size=8)
+        with self.session.as_default():
+            with self.graph.as_default():
+                probs = self.model.predict(x, batch_size=8)
         preds = np.argmax(probs, axis=-1)
         pred_labels = []
         for i, pred in enumerate(preds):
             pred_labels.append([inv_label_map[t]
                                 for t in pred[1:len(data.tokens[i])+1]])
         lines = write_result(
-            'output.tsv', data.words, data.lengths,
+            '/dev/null', data.words, data.lengths,
             data.tokens, data.labels, pred_labels, mode='predict'
         )
         return ''.join(lines)
@@ -70,9 +115,7 @@ class Tagger(object):
         with graph.as_default():
             with session.as_default():
                 model, tokenizer, labels, config = load_ner_model(model_dir)
-                tagger = cls(model, tokenizer, labels, config)
-                tagger.session = session
-                tagger.graph = graph
+                tagger = cls(session, graph, model, tokenizer, labels, config)
         return tagger
 
 
@@ -95,7 +138,11 @@ def main(argv):
     args = argparser.parse_args(argv[1:])
     if args.ner_model_dir is None:
         args.ner_model_dir = DEFAULT_MODEL_DIR
-    app.tagger = Tagger.load(args.ner_model_dir)
+    app.tagger_thread = TaggerThread(args.ner_model_dir)
+    app.tagger_thread.start()
+    print("Loading model...")
+    app.tagger_thread.ping()
+    print("Model loaded. Starting server.")
     app.run(host=args.host, port=args.port)
     return 0
 
